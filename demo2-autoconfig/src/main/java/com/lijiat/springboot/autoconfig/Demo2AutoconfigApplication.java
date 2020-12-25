5d4f1191cbba5c75f29ef026c024e340f75f4c4c
lijiat-lin <lijiat_lin@163.com>
2020-12-25 19:00:19 +0800

    springboot 自动配置

---
+++ demo2-autoconfig/src/main/java/com/lijiat/springboot/autoconfig/Demo2AutoconfigApplication.java
@@ -0,0 +1,13 @@
+package com.lijiat.springboot.autoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2AutoconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2AutoconfigApplication.class, args);
+    }
+
+}
---
+++ demo2-autoconfig/src/test/java/com/lijiat/springboot/autoconfig/Demo2AutoconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lijiat.springboot.autoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2AutoconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
